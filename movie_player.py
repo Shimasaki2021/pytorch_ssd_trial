4468a204c4db19e82f584018956b1582ea8d3a08
Shimasaki2021 <shimasaki.takamasa@gmail.com>
2025-01-12 12:25:30 +0000

    動画→画像変換ツール登録

---
+++ movie_player.py
@@ -0,0 +1,78 @@
+import sys
+import cv2
+import numpy
+import os
+
+from common_ssd import ImageProc
+
+
+def main(movie_fpath:str, play_fps:float, img_proc:ImageProc):
+
+    # 画像出力用フォルダ作成
+    output_imgdir_name:str = os.path.splitext(os.path.basename(movie_fpath))[0]
+    output_imgdir_path:str = f"./test_data/{img_proc.toString()}/" + output_imgdir_name
+
+    if os.path.isfile(movie_fpath) == False:
+        print("Error: ", movie_fpath, " is nothing.")
+    else:
+
+        if os.path.isdir(output_imgdir_path) == False:
+            os.makedirs(output_imgdir_path)
+
+            
+        # 入力動画読み込み
+        cap = cv2.VideoCapture(movie_fpath)
+        num_frame:int = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        cap_fps:float = cap.get(cv2.CAP_PROP_FPS)
+
+        frame_play_step:int = int((cap_fps + 0.1) / play_fps)
+        if frame_play_step < 1:
+            frame_play_step = 1
+
+        frame_no:int = 0
+
+        while frame_no < num_frame:
+
+            # frame読み込み
+            frame_no = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+            frame_img:numpy.ndarray = None
+            (_, frame_img) = cap.read()
+
+            if frame_no % frame_play_step == 0:
+
+                if not (frame_img is None):
+
+                    frame_img_cliped = img_proc.clip(frame_img)
+
+                    # 保存
+                    frame_img_fpath = f"{output_imgdir_path}/{output_imgdir_name}_{img_proc.toString()}_F{frame_no:05}.jpg" 
+                    cv2.imwrite(frame_img_fpath, frame_img_cliped)
+
+                    print(f"Image saved: {frame_img_fpath}")
+
+                    # ImageProc.draw_text(frame_img, "F{:05}".format(frame_no), (0, 30), 0.75, (255,255,255), 2, True)
+                    
+            #         # 表示
+            #         cv2.imshow(output_imgdir_name, frame_img_cliped)
+
+            # key:int = cv2.waitKey(1) & 0xFF
+            # if key == ord("q"):
+            #     break           
+
+    return
+
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    # img_proc = ImageProc(500, 250, 1100, 500)
+    img_proc = ImageProc(680, 250, 1280, 550)
+    play_fps:float = -1.0
+
+    if len(args) < 2:
+        print("Usage: ", args[0], " [movie file path] ([play fps])")
+    else:
+        if len(args) >= 3:
+            play_fps = float(args[2])
+
+        main(args[1], play_fps, img_proc)
